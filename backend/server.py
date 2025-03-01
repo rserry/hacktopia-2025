4762fe63a001af2193b650e72fbce3821df02ac8
robinvanlooveren <robin.vanlooveren@student.kuleuven.be>
2025-03-01 12:43:26 +0100

    fastapi initial commit

--- backend/server.py
+++ backend/server.py
@@ -1,9 +1,16 @@
 from fastapi import FastAPI, HTTPException
+from fastapi.middleware.cors import CORSMiddleware
 from pydantic import BaseModel
 from typing import List
-from constraint-alg import calculate_result
+# from constraint_alg import calculate_result
 
 app = FastAPI()
+
+app.add_middleware(
+    CORSMiddleware,
+    allow_origins=['*'],
+    allow_methods=["GET", "POST", "PUT", "DELETE"]
+)
 
 class InputData(BaseModel):
     preferred_categories: List[str]
@@ -12,6 +19,10 @@ class InputData(BaseModel):
     budget: float
     target_calories: float
     target_protein: float
+
+def calculate_result(preferred_categories: list, preferred_crops: list, disliked_crops: list, budget: float, target_calories: float, target_protein: float):
+    result: tuple[str, dict[str, int]] = ("A", {"B": 10, "C": 20})
+    return result
 
 @app.post("/calculate")
 async def calculate(input_data: InputData):
